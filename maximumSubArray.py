8984cea54560eec40bd090a93a171001fdd64551
SM ABDULLAH <sm.abdullah@monash.edu>
2019-03-04 10:38:24 +1100

    PEP 8 is followed to change the function and variable names

--- maximumSubArray.py
+++ maximumSubArray.py
@@ -4,48 +4,50 @@ S M Abdullah (sma.csedu@gmail.com)
 27-02-2019
 
 The aim of this program is to find the maximum subarray from an array of values.
+
+04/03/2019: Function and variable names are updated following PEP 8
 """
 
 # Importing library
 import math
 
-def findMaxCrossingSubArray(A, low, mid, high):
-    leftSum = -math.inf
+def find_max_crossing_subarray(A, low, mid, high):
+    left_sum = -math.inf
     sums = 0
-    maxLeft = 0
-    for i in range(mid,low+1,-1):
+    max_left = 0
+    for i in range(mid, low+1, -1):
         sums = sums + A[i]
-        if sums > leftSum:
-            leftSum = sums
-            maxLeft = i
-    rightSum = - math.inf
+        if sums > left_sum:
+            left_sum = sums
+            max_left = i
+    right_sum = - math.inf
     sums = 0
-    maxRight = 0
-    for j in range(mid+1,high+1):
+    max_right = 0
+    for j in range(mid+1, high+1):
         sums = sums + A[j]
-        if sums > rightSum:
-            rightSum = sums
-            maxRight = j
-    return [maxLeft, maxRight,leftSum+rightSum]
+        if sums > right_sum:
+            right_sum = sums
+            max_right = j
+    return [max_left, max_right, left_sum + right_sum]
 
-def findMaximumSubArray(A, low, high):
+def find_maximum_subarray(A, low, high):
     if high == low:
         # base-case. Only one item in the list
         return [low, high, A[low]]
     else:
         # divide the list into two parts
-        mid = (low+high)//2
-    leftResult = findMaximumSubArray(A,low,mid)
-    rightResult = findMaximumSubArray(A, mid+1, high)
+        mid = (low + high) // 2
+    left_result = find_maximum_subarray(A, low, mid)
+    right_result = find_maximum_subarray(A, mid+1, high)
     # the maximum is crossing between two parts
-    crossResult = findMaxCrossingSubArray(A, low, mid, high)
-    if leftResult[2] >= rightResult[2] and leftResult[2] >= crossResult[2]:
-        return leftResult
-    elif rightResult[2] >= leftResult[2] and rightResult[2] >= crossResult[2]:
-        return rightResult
+    cross_result = find_max_crossing_subarray(A, low, mid, high)
+    if left_result[2] >= right_result[2] and left_result[2] >= cross_result[2]:
+        return left_result
+    elif right_result[2] >= left_result[2] and right_result[2] >= cross_result[2]:
+        return right_result
     else:
-        return crossResult
+        return cross_result
 # test data
 A = [13,-3,-25,20,-3,-16,-23,18,20,-7,12,-5,-22,15,-4,7]
-result = findMaximumSubArray(A, 0, len(A)-1)
-print('The optimal subarray is from index {:d} to index {:d} is {:d}'.format(result[0]+1, result[1]+1, result[2]))
+result = find_maximum_subarray(A, 0, len(A) - 1)
+print('The optimal subarray is from index {:d} to index {:d} is {:d}'.format(result[0] + 1, result[1] + 1, result[2]))
